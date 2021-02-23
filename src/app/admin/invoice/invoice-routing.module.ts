0ef384196d6aa076bb9de5842fa6dba9abedb04e
Nipuna Nanayakkara <Nipuna.Nanayakkara@volume.co.uk>
2021-02-24 02:10:04 +0530

    invoice view and other fixes

--- src/app/admin/invoice/invoice-routing.module.ts
+++ src/app/admin/invoice/invoice-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { InvoiceComponent } from './invoice.component';
 
-const routes: Routes = [{ path: '', component: InvoiceComponent }];
+const routes: Routes = [
+  { path: '', component: InvoiceComponent },
+  { path: 'view-invoice', loadChildren: () => import('./view-invoice/view-invoice.module').then(m => m.ViewInvoiceModule) }, 
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
